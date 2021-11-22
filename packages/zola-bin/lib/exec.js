d3f117f5cad34161caa1a4ee893eb608700027dd
Chanakya <ucs3721@outlook.com>
2021-11-22 23:57:25 +0530

    v0.0.6 - Now, works with multiple arguments

--- packages/zola-bin/lib/exec.js
+++ packages/zola-bin/lib/exec.js
@@ -1,6 +1,10 @@
 import { execFileSync } from "child_process";
 import { getZolaPath } from "./path.js";
 
-export function exec(...args) {
+/**
+ * If arguments are in a Array
+ * @param {string[]} args
+ */
+export function exec(args) {
   execFileSync(getZolaPath(), args, { stdio: "inherit" });
 }
