73102f0738efbeee392735296cc0d11e0c8fb767
Chanakya <ucs3721@outlook.com>
2021-12-23 19:56:25 +0530

    Fix async `exec` and make it return a promise

--- packages/zola-bin/lib/exec.ts
+++ packages/zola-bin/lib/exec.ts
@@ -1,25 +1,23 @@
 import { execFile, execFileSync } from "child_process";
+import { promisify } from "util";
 import { getZolaPath } from "./path.js";
 
+// https://nodejs.org/api/child_process.html#child_processexecfilefile-args-options-callback
+const execPromise = promisify(execFile);
+
 /**
- * Calls zola binary file with the given arguments Asynchronously.
+ * Calls zola binary file with the given arguments Asynchronously. Returns a promise.
  *
  * Reference: https://www.getzola.org/documentation/getting-started/cli-usage/
  * @param args as a array of strings
  */
 export function exec(args: string[]) {
-	return execFile(getZolaPath(), args);
+	return execPromise(getZolaPath(), args);
 }
 
 /**
- * Sync version of exec.
+ * Sync version of exec. Returns nothing, unlike its async counterpart.
  */
 export function execSync(args: string[]) {
 	execFileSync(getZolaPath(), args, { stdio: "inherit" });
 }
-
-// declare function execFile(
-// 	file: string,
-// 	args: string[],
-// 	callback: (error: Error | null, stdout: string, stderr: string) => void
-// ): ChildProcess;
