92837923935fb620decce5f65610d4942e656587
Chanakya Srinivas <chanakya.srinivas@unifize.com>
2026-01-25 12:38:15 +0530

    Add target option to build.js

--- utils/build.mjs
+++ utils/build.mjs
@@ -2,6 +2,7 @@ import { NapiCli } from "@napi-rs/cli";
 
 const buildOptions = {
 	release: process.env.NODE_ENV === "prod",
+	target: process.env.NAPI_BUILD_TARGET || undefined,
 };
 
 console.log("Building with options:", buildOptions);
@@ -9,6 +10,7 @@ console.log("Building with options:", buildOptions);
 new NapiCli().build({
 	platform: true,
 	release: buildOptions.release,
+	target: buildOptions.target,
 	manifestPath: "./zola/Cargo.toml",
 	jsBinding: "packages/zola-bin/bindings.js",
 	dts: "packages/zola-bin/bindings.d.ts",
