8c54669565735175226a9f862c5b3fedd1e23680
Chanakya Srinivas <chanakya.srinivas@unifize.com>
2026-01-25 12:25:17 +0530

    Support release build from build.js

--- utils/build.mjs
+++ utils/build.mjs
@@ -1,12 +1,19 @@
 import { NapiCli } from "@napi-rs/cli";
 
-// const args = process.argv;
+const buildOptions = {
+	release: process.env.NODE_ENV === "prod",
+};
+
+console.log("Building with options:", buildOptions);
 
 new NapiCli().build({
 	platform: true,
+	release: buildOptions.release,
 	manifestPath: "./zola/Cargo.toml",
 	jsBinding: "packages/zola-bin/bindings.js",
 	dts: "packages/zola-bin/bindings.d.ts",
 	jsPackageName: "@u-c-s/zola",
 	outputDir: "./",
 });
+
+// napi build --platform --release --manifest-path ./zola/Cargo.toml --dts packages/zola-bin/bindings.d.ts --js packages/zola-bin/bindings.js --js-package-name @u-c-s/zola -o ./
